4d0f4ef0b0a50d4fcbd65b25247844334a1c150b
kingkonghi <kingkonghi12@gmail.com>
2022-03-22 15:22:52 +0800

    Updated on 22/03/2022

--- ML.py
+++ ML.py
@@ -1,9 +1,14 @@
-import numpy as np
-import pandas as pd
-from sklearn.preprocessing import Imputer
+#-*-coding:utf-8-*-
 
-dataset = pd.read_csv('Data.csv');
-X = dataset.iloc[ : , :-1].values
-Y = dataset.iloc[ : , 3].values
+dic = {'Herman':18,'Kitty':16,'Teresa':50}
 
+dic['Dad'] = 52
 
+set1 = set([1,2,3,3,4,4,3,5])
+set2 = set([1,2,4,6,7])
+set3 = set2 - set1
+print(set3)
+if (len(set1) ==0):
+    print("True")
+else:
+    print("Else")
